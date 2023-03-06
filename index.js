45cfc366c24d7257e6604121c2c2145d57b30593
malconn <malconaugusto@outlook.com>
2023-03-06 19:00:28 -0300

    feat: arquitetura e organização de aplicação Node.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const connection = require('./database/database');
+const connection = require("./database/database");
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
 
 // View engine
 app.set("view engine", "ejs");
@@ -16,18 +18,20 @@ app.use(bodyParser.json());
 // Database
 connection
   .authenticate()
-  .then(()=>{
+  .then(() => {
     console.log("Connection has been established successfully.");
   })
-  .catch((error)=>{
+  .catch((error) => {
     console.log(error);
-  })
+  });
 
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
   console.log("Server is running on port 8080!");
-})
+});
